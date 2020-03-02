c9d8f9d1e2e87b80efbddd5677c6ae4a9d8c145a
Mikhail Rozdin <mrozdin@yandex.ru>
2020-03-02 18:36:09 +0300

    rabbit confirm notify

--- eventscan/src/main/java/io/lastwill/eventscan/messages/TransferConfirmNotify.java
+++ eventscan/src/main/java/io/lastwill/eventscan/messages/TransferConfirmNotify.java
@@ -5,14 +5,12 @@ import lombok.ToString;
 
 @Getter
 @ToString(callSuper = true)
-public class DucTransferedNotify extends BaseNotify {
-    private final String ducAddress;
+public class TransferConfirmNotify extends BaseNotify {
     private final long transferId;
     private final boolean isSuccess;
 
-    public DucTransferedNotify(String ducAddress, String txHash, long transferId, boolean isSuccess) {
+    public TransferConfirmNotify(String txHash, long transferId, boolean isSuccess) {
         super(PaymentStatus.COMMITTED, txHash);
-        this.ducAddress = ducAddress;
         this.transferId = transferId;
         this.isSuccess = isSuccess;
     }
--- eventscan/src/main/java/io/lastwill/eventscan/services/handlers/TransitionEventHandler.java
+++ eventscan/src/main/java/io/lastwill/eventscan/services/handlers/TransitionEventHandler.java
@@ -1,18 +1,14 @@
 package io.lastwill.eventscan.services.handlers;
 
 import io.lastwill.eventscan.events.model.TransferConfirmEvent;
-import io.lastwill.eventscan.messages.DucTransferedNotify;
+import io.lastwill.eventscan.messages.TransferConfirmNotify;
 import io.lastwill.eventscan.services.ExternalNotifier;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.event.EventListener;
-import org.springframework.stereotype.Component;
 
 @Slf4j
-@Component
-@ConditionalOnBean(ExternalNotifier.class)
-public class DucExchangeEventHandler {
+public class TransitionEventHandler {
     @Autowired
     private ExternalNotifier externalNotifier;
 
@@ -21,14 +17,13 @@ public class DucExchangeEventHandler {
         try {
             externalNotifier.send(
                     event.getNetworkType(),
-                    new DucTransferedNotify(
-                          event.getDucAddress(),
-                            event.getTx_hash(),
+                    new TransferConfirmNotify(
+                            event.getTxHash(),
                             event.getTransferId(),
                             event.isSuccess())
             );
         } catch (Throwable e) {
-            log.error("Sending notification about new Ducatus exchange failed.", e);
+            log.error("Sending notification about transition confirmed are failed.", e);
         }
     }
 }
