da23b976fb409d37130bd8ade2093484e1e5586a
Mikhail Rozdin <mrozdin@yandex.ru>
2020-03-04 17:34:06 +0300

    drop unused methods from repository

--- mywish-model/src/main/java/io/lastwill/eventscan/repositories/ExchangeRepository.java
+++ mywish-model/src/main/java/io/lastwill/eventscan/repositories/ExchangeRepository.java
@@ -2,7 +2,6 @@ package io.lastwill.eventscan.repositories;
 
 import io.lastwill.eventscan.model.CryptoCurrency;
 import io.lastwill.eventscan.model.Exchange;
-import io.lastwill.eventscan.model.TransactionStatus;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -12,10 +11,6 @@ import java.util.Collection;
 import java.util.List;
 
 public interface ExchangeRepository extends CrudRepository<Exchange, Long> {
-    List<Exchange> findAllByStatus(@Param("status") TransactionStatus status);
-
-    @Query("select c from Exchange c where c.receiveAddress in :receiveAddress and c.fromCurrency = :fromCurrency")
+    @Query("select c from Exchange c where c.receiveAddress = :receiveAddress and c.fromCurrency = :fromCurrency")
     List<Exchange> findByRxAddress(@Param("receiveAddress") Collection<String> receiveAddress,@Param("fromCurrency") CryptoCurrency fromCurrency);
-
-    //List<Exchange> getUserAddressExchange(@Param("status") TransactionStatus status);
 }
--- mywish-model/src/main/java/io/lastwill/eventscan/repositories/TransferRepository.java
+++ mywish-model/src/main/java/io/lastwill/eventscan/repositories/TransferRepository.java
@@ -2,21 +2,11 @@ package io.lastwill.eventscan.repositories;
 
 import io.lastwill.eventscan.model.TransactionStatus;
 import io.lastwill.eventscan.model.Transfer;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
-import java.math.BigInteger;
-import java.util.Collection;
 import java.util.List;
 
 public interface TransferRepository extends CrudRepository<Transfer, Long> {
-    @Query("select c from Transfer c where c.status in :status")
-    List<Transfer> findByStatus(@Param("status") TransactionStatus status);
-
-    @Query("select c from Transfer c where c.exchangeId in :exchangeId")
-    List<Transfer> findByExchangeId(@Param("exchangeId") Collection<Long> exchangeId);
-
     List<Transfer> findAllByStatus(@Param("status") TransactionStatus status);
-
 }
